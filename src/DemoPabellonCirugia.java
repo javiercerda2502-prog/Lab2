0cdbad8a4d568a09ee2f584bfdbee9aa509324ba
javiercerda2502-prog <javier.cerda2502@alumnos.ubiobio.cl>
2026-04-14 19:42:27 -0400

    NO se pusheo cuando lo temia que mandar

--- src/DemoPabellonCirugia.java
+++ src/DemoPabellonCirugia.java
@@ -1,15 +1,84 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+import java.util.Scanner;
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+public class DemoPabellonCirugia {
+
+    PabellonCirugia[] pabellonCirugias = new PabellonCirugia[6];
+
+    public static void main(String[] args) {
+
+        DemoPabellonCirugia demo = new DemoPabellonCirugia();
+
+        demo.Procesa();
+
+    }
+
+    public void Procesa() {
+
+        Scanner sc = new Scanner(System.in);
+
+        for (int i = 0; i < pabellonCirugias.length; i++) {
+
+            System.out.println("Pabellon "+ (i+1));
+
+            System.out.print("Ingrese el numero de pabellon: ");
+            int numero = sc.nextInt();
+
+            if (numero < 1) {
+
+                break;
+
+            }
+
+            System.out.print("Ingrese la especialidad del pabellon: ");
+            String especialidad = sc.next();
+
+            pabellonCirugias[i] = new PabellonCirugia(numero, especialidad);
+
+            if (i == 2 || i == 4 || i == 0) {
+
+                pabellonCirugias[i].setEstado(Estado.OCUPADO);
+
+            } else {
+
+                pabellonCirugias[i].setEstado(Estado.DISPONIBLE);
+
+            }
+
         }
+
+        System.out.println();
+
+        System.out.println("Pabellones cirugias:");
+
+        for (int i = 0; i < pabellonCirugias.length; i++) {
+
+            System.out.println(pabellonCirugias[i]);
+
+        }
+
+        System.out.println();
+
+        boolean existeigual = false;
+
+        for (int i = 0; i < pabellonCirugias.length; i++) {
+
+            for (int j = 1; j < pabellonCirugias.length; j++) {
+
+                if (pabellonCirugias[i].equals(pabellonCirugias[j])) {
+
+                    System.out.println("Posiciones: "+i+" y "+j);
+                    existeigual = true;
+
+                }
+            }
+        }
+
+        if (!existeigual) {
+
+            System.out.println("No existen iguales");
+
+        }
+
+
     }
 }
--- src/PabellonCirugia.java
+++ src/PabellonCirugia.java
@@ -1,4 +1,54 @@
-package PACKAGE_NAME;
+import java.util.Objects;
 
 public class PabellonCirugia {
+
+    private int numero;
+    private String especialidad;
+    private Estado estado;
+
+    public PabellonCirugia(int numero, String especialidad) {
+
+        this.numero = numero;
+        this.especialidad = especialidad;
+        this.estado = Estado.DISPONIBLE;
+
+    }
+
+    public int getNumero() {
+        return numero;
+    }
+
+    public String getEspecialidad() {
+        return especialidad;
+    }
+
+    public Estado getEstado() {
+        return estado;
+    }
+
+    public void setEstado(Estado estado) {
+        this.estado = estado;
+    }
+
+    @Override
+    public String toString() {
+
+        String estadoString = (estado == Estado.OCUPADO) ? "ocupado" : "disponible";
+
+        return "PabellonCirugia { " +
+                "numero =" + numero +
+                ", especialidad ='" + especialidad + '\'' +
+                ", estado =" + estadoString +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        PabellonCirugia otro = (PabellonCirugia) o;
+        return this.numero == otro.numero;
+    }
+
 }
